3b590ec36010a534e2710ded8f2c1cf51f6d482f
GavG <gavingration@gmail.com>
2020-11-26 22:17:26 +0000

    wip

--- mtu_sizer.js
+++ mtu_sizer.js
@@ -1,24 +1,33 @@
 net = require('net');
 
+let max_mtu = 0
+
 //Create the TCP listener
 net.createServer(function (socket) {
 
-    //Make a normal looking https response
-    //Headers
-    socket.write([
-        'HTTP/1.1 200 OK',
-        'Content-Type: text/html; charset=UTF-8',
-        'Content-Encoding: UTF-8',
-        'Accept-Ranges: bytes',
-        'Access-Control-Allow-Origin: *',
-    ].join('\n') + '\n\n')
-
-    //Body
+    //Makes a normal looking https response
     socket.on("data", function (data) {
-        //Send back the MTU once we have data to calculate it
-        socket.write(JSON.stringify({mtu: data.length}))
-        socket.end()
+        
+        //Watch the incoming TCP chunks over some time, find the longest
+        if (data.length > max_mtu) {
+            max_mtu = data.length
+        }
     })
+
+    setTimeout(function () {
+        //Headers
+        socket.write([
+            'HTTP/1.1 200 OK',
+            'Content-Type: text/html; charset=UTF-8',
+            'Content-Encoding: UTF-8',
+            'Accept-Ranges: bytes',
+            'Access-Control-Allow-Origin: *',
+        ].join('\n') + '\n\n')
+        //Body
+        socket.write(JSON.stringify({ mtu: max_mtu }))
+
+        socket.end()
+    }, 500)
 
     socket.on("error", function(err) {
         //discard errors, attempts to write to the socket after we've done our thing
