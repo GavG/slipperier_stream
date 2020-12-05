83948af855e174149830e182a42fd069590616df
GavG <gavingration@gmail.com>
2020-12-05 14:03:10 +0000

    standalone site server

--- site_server.js
+++ site_server.js
@@ -1,4 +1,10 @@
-var express = require('express');
-var app = express();
-app.use('/', express.static(__dirname + '/../public'));
-app.listen(80, function () { console.log('listening'); });
+const args = process.argv;
+
+var express = require('express')
+var app = express()
+
+app.use('/', express.static(__dirname + '/public'))
+
+app.listen(args[2] || 80, function () {
+    console.log('listening')
+})
