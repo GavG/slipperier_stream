19f75bb3beebca303c63691cdf136fbd9ae00c38
GavG <gavingration@gmail.com>
2020-12-05 15:16:05 +0000

    wip

--- mtu_sizer.js
+++ mtu_sizer.js
@@ -6,19 +6,22 @@ let clients = {}
 net.createServer(function (socket) {
 
     socket.on("data", function (data) {
-        
         //Watch the incoming TCP chunks over some time, find the longest, per IP (client)
+
         if (!clients[socket.remoteAddress]) {
             clients[socket.remoteAddress] = {
                 mtu: 0,
-                response_timeout: null,
+                captures: 0,
             }
         }
 
+        clients[socket.remoteAddress].captures += 1
+
         if (data.length > clients[socket.remoteAddress].mtu) {
-            clients[socket.remoteAddress] = {
-                mtu: data.length,
-            }
+            clients[socket.remoteAddress].mtu = data.length
+        }
+
+        if (clients[socket.remoteAddress].captures > 1) {
             respondWithMtu(socket)
         }
     })
@@ -30,34 +33,28 @@ net.createServer(function (socket) {
 }).listen(9090)
 
 function respondWithMtu(socket) {
-    //Return a normal looking http resp after some time
-    if (clients[socket.remoteAddress].response_timeout) {
-        clearTimeout(clients[socket.remoteAddress].response_timeout)
-    }
+    //Return a normal looking http resp
 
-    clients[socket.remoteAddress].response_timeout = responder_timeout = setTimeout(() => {
-        //Headers
-        socket.write([
-            'HTTP/1.1 200 OK',
-            'Content-Type: text/html; charset=UTF-8',
-            'Content-Encoding: UTF-8',
-            'Accept-Ranges: bytes',
-            'Access-Control-Allow-Origin: *',
-        ].join('\n') + '\n\n')
+    //Headers
+    socket.write([
+        'HTTP/1.1 200 OK',
+        'Content-Type: text/html; charset=UTF-8',
+        'Content-Encoding: UTF-8',
+        'Accept-Ranges: bytes',
+        'Access-Control-Allow-Origin: *',
+    ].join('\n') + '\n\n')
 
-        //Body
-        socket.write(JSON.stringify({ 
-            mtu: clients[socket.remoteAddress].mtu
-        }))
+    //Body
+    socket.write(JSON.stringify({ 
+        mtu: clients[socket.remoteAddress].mtu
+    }))
 
-        console.log('RESP to IP: ' + socket.remoteAddress + ' MTU: ' + clients[socket.remoteAddress].mtu)
+    console.log('RESP to IP: ' + socket.remoteAddress + ' MTU: ' + clients[socket.remoteAddress].mtu)
 
-        //Unset the client data
-        delete clients[socket.remoteAddress]
+    //Unset the client data
+    delete clients[socket.remoteAddress]
 
-        socket.end()
-
-    }, 500)
+    socket.end()
 }
 
 console.log('ready')
